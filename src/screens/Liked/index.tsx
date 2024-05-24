87e15a249bd9152d9ffbdacba35bed593150ea20
Jashwanth459 <jaswanth.puligundla@gmail.com>
2024-05-23 23:59:10 -0500

    deploy to gh-pages

--- src/screens/Liked/index.tsx
+++
@@ -1,3 +0,0 @@
-import Liked from './Liked'
-
-export { Liked }
--- src/screens/index.ts
+++
@@ -1,6 +0,0 @@
-import { Cart } from '../screens/Cart'
-import { Home } from '../screens/Home'
-import { Liked } from '../screens/Liked'
-import { Profile } from '../screens/Profile'
-
-export { Cart, Home, Liked, Profile }
